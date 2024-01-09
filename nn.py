0f9e171e87eceb5f5824d7c3426025388c790e8f
Obada Khalili <obada.akhalili@gmail.com>
2024-01-09 19:26:04 +0200

    implement initial '.backward()' tensor logic

--- nn.py
+++ nn.py
@@ -2,30 +2,35 @@ from petitegrad.tensor import Tensor
 from mnist import MNIST
 import numpy as np
 
-# np.random.seed(1337)
-
 
 class PetiteNet:
     def __init__(self, input_size, output_size):
-        h1_size = 128
+        h1_size = 10
         h2_size = 64
+
         self.w1 = Tensor(np.random.randn(input_size, h1_size))
-        self.b1 = Tensor(np.random.randn(h1_size))
+        self.b1 = Tensor(np.random.rand(h1_size))
         self.w2 = Tensor(np.random.randn(h1_size, h2_size))
-        self.b2 = Tensor(np.random.randn(h2_size))
+        self.b2 = Tensor(np.random.rand(h2_size))
         self.w3 = Tensor(np.random.randn(h2_size, output_size))
-        self.b3 = Tensor(np.random.randn(output_size))
+        self.b3 = Tensor(np.random.rand(output_size))
+
+        self.params = [self.w1, self.b1, self.w2, self.b2, self.w3, self.b3]
 
     def forward(self, X):
+        assert isinstance(X, Tensor)
+        assert X.data.ndim == 2
+
         return (
-            X.mul(self.w1)
+            X.matmul(self.w1)
             .add(self.b1)
             .relu()
-            .mul(self.w2)
+            .matmul(self.w2)
             .add(self.b2)
             .relu()
-            .mul(self.w3)
+            .matmul(self.w3)
             .add(self.b3)
+            .sigmoid()
         )
 
 
@@ -47,15 +52,33 @@ if __name__ == "__main__":
     print("Fetching MNIST data...")
     X_train, y_train, X_test, y_test = fetch_mnist()
 
+    X_test = Tensor(X_test)
+
     input_size = X_train.data.shape[1]
     output_size = 10
+    net = PetiteNet(input_size, output_size)
 
     batch_size = 64
-    X_batch = Tensor(X_train[:batch_size])
-    y_batch = Tensor(y_train[:batch_size])
+    for epoch in range(1000):
+        batches_indices = np.arange(len(X_train))
+        np.random.shuffle(batches_indices)
 
-    net = PetiteNet(input_size, output_size)
-    output = net.forward(X_batch)
-    loss = output.mean_squared_error(y_batch)
+        for batch_indices in [
+            batches_indices[batch_idx : batch_idx + batch_size]
+            for batch_idx in range(0, len(batches_indices), batch_size)
+        ]:
+            X_batch = Tensor(X_train[batch_indices])
+            y_batch = Tensor(y_train[batch_indices])
 
-    loss.backward()
+            output = net.forward(X_batch)
+            loss = output.mean_squared_error(y_batch)
+
+            loss.backward()
+
+            for p in net.params:
+                p.data -= 0.01 * p.grad
+
+        if epoch % 10 == 0:
+            predictions = net.forward(X_test)
+            accuracy = (predictions.data.argmax(axis=1) == y_test.argmax(axis=1)).mean()
+            print(f"Epoch {epoch}: accuracy = {accuracy}")
--- petitegrad/tensor.py
+++ petitegrad/tensor.py
@@ -2,43 +2,92 @@ import numpy as np
 
 
 class Tensor:
-    def __init__(self, nparray):
-        assert isinstance(nparray, np.ndarray)
-        self.data = nparray
+    def __init__(self, data, grad_fn=None, src=None):
+        assert isinstance(data, np.ndarray)
+        assert grad_fn is None or callable(grad_fn)
+        assert src is None or isinstance(src, list)
+        if src is not None:
+            assert all(isinstance(t, Tensor) for t in src)
 
-    def transpose(self):
-        return Tensor(self.data.transpose())
+        self.data = data
+        self.grad = None
+        self._grad_fn = grad_fn
+        self._src = src
 
-    def mul(self, tensor):
-        assert isinstance(tensor, Tensor)
-        return Tensor(self.data @ tensor.data)
+    def matmul(self, t):
+        assert isinstance(t, Tensor)
+        assert self.data.ndim == t.data.ndim == 2
+        assert self.data.shape[1] == t.data.shape[0]
 
-    def add(self, tensor):
-        assert isinstance(tensor, Tensor)
-        return Tensor(self.data + tensor.data)
+        def matmul_grad():
+            # TODO: not sure if this is correct (dividing by batch size). I mean abstraction wise
+            self.grad = (out.grad @ t.data.T) / self.data.shape[0]
+            t.grad = (self.data.T @ out.grad) / self.data.shape[0]
+
+        out = Tensor(self.data @ t.data, grad_fn=matmul_grad, src=[self, t])
+
+        return out
+
+    # NOTE: consider whether operands should be validated
+    def add(self, t):
+        assert isinstance(t, Tensor)
+
+        def add_grad():
+            self.grad = np.ones_like(self.data) * out.grad
+            t.grad = np.ones_like(t.data) * out.grad
+
+            # TODO: not sure this is correct. I mean abstraction wise
+            if self.data.shape != t.data.shape:
+                if self.data.ndim > t.data.ndim:
+                    t.grad = t.grad.sum(axis=0)
+                else:
+                    self.grad = self.grad.sum(axis=0)
+
+        out = Tensor(self.data + t.data, grad_fn=add_grad, src=[self, t])
+
+        return out
 
     def relu(self):
-        return Tensor(np.maximum(self.data, 0))
+        def relu_grad():
+            self.grad = (self.data > 0) * out.grad
+
+        out = Tensor(np.maximum(self.data, 0), grad_fn=relu_grad, src=[self])
+
+        return out
 
     def sigmoid(self):
-        return Tensor(1 / (1 + np.exp(-self.data)))
+        def sigmoid_grad():
+            self.grad = out.grad * out.data * (1 - out.data)
 
-    def softmax(self):
-        exps = np.exp(self.data)
-        return Tensor(exps / np.sum(exps, axis=1, keepdims=True))
+        out = Tensor(1 / (1 + np.exp(-self.data)), grad_fn=sigmoid_grad, src=[self])
 
-    def categorical_crossentropy(self, tensor):
-        assert isinstance(tensor, Tensor)
-        probs = self.softmax()
-        return Tensor(
-            -np.sum(tensor.data * np.log(probs.data), axis=1).mean(keepdims=True)
+        return out
+
+    # NOTE: consider whether operands should be validated
+    def mean_squared_error(self, y):
+        assert isinstance(y, Tensor)
+
+        def mse_grad():
+            self.grad = (self.data - y.data) * out.grad
+            y.grad = (y.data - self.data) * out.grad
+
+        out = Tensor(
+            (np.square(self.data - y.data) / 2).sum(axis=1).mean(keepdims=True),
+            grad_fn=mse_grad,
+            src=[self, y],
         )
 
-    def mean_squared_error(self, tensor):
-        assert isinstance(tensor, Tensor)
-        return Tensor(
-            (np.square(self.data - tensor.data) / 2).sum(axis=1).mean(keepdims=True)
-        )
+        return out
 
     def backward(self):
-        pass
+        if self._grad_fn is None:
+            return
+
+        if self.grad is None:
+            self.grad = 1
+
+        self._grad_fn()
+
+        if self._src is not None:
+            for t in self._src:
+                t.backward()

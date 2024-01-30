b6cb5222c1c1f6d09357916619d60f6658fab3e5
Obada Khalili <obada.akhalili@gmail.com>
2024-01-30 16:58:51 +0200

    reimplement tensor class

--- petitegrad/tensor.py
+++ petitegrad/tensor.py
@@ -3,91 +3,127 @@ import numpy as np
 
 class Tensor:
     def __init__(self, data, grad_fn=None, src=None):
-        assert isinstance(data, np.ndarray)
-        assert grad_fn is None or callable(grad_fn)
-        assert src is None or isinstance(src, list)
-        if src is not None:
-            assert all(isinstance(t, Tensor) for t in src)
+        if isinstance(data, (int, float)):
+            data = np.array(data, dtype=np.float32)
 
-        self.data = data
-        self.grad = None
+        if isinstance(data, np.ScalarType) and (
+            np.issubdtype(data.dtype, np.integer)
+            or np.issubdtype(data.dtype, np.floating)
+        ):
+            data = np.array(data, dtype=np.float32)
+
+        if isinstance(data, np.ndarray) and np.issubdtype(data.dtype, np.integer):
+            data = data.astype(np.float32)
+
+        assert isinstance(data, np.ndarray) and np.issubdtype(
+            data.dtype, np.floating
+        ), "data must be a scalar or numpy array of floats or integers"
+        assert grad_fn is None or callable(grad_fn), "grad_fn must be callable"
+        assert src is None or isinstance(src, list), "src must be a list of tensors"
+        if src is not None:
+            assert all(
+                isinstance(t, Tensor) for t in src
+            ), "src must be a list of tensors"
+
+        self._data = data
+        self._grad = np.zeros_like(data, dtype=data.dtype)
         self._grad_fn = grad_fn
         self._src = src
 
-    def matmul(self, t):
-        assert isinstance(t, Tensor)
-        assert self.data.ndim == t.data.ndim == 2
-        assert self.data.shape[1] == t.data.shape[0]
+    @property
+    def data(self):
+        return self._data
 
-        def matmul_grad():
-            # TODO: not sure if this is correct (dividing by batch size). I mean abstraction wise
-            self.grad = (out.grad @ t.data.T) / self.data.shape[0]
-            t.grad = (self.data.T @ out.grad) / self.data.shape[0]
+    @property
+    def grad(self):
+        return self._grad
 
-        out = Tensor(self.data @ t.data, grad_fn=matmul_grad, src=[self, t])
+    def mul(self, t):
+        if isinstance(t, (int, float)):
+            t = Tensor(t)
 
-        return out
+        assert isinstance(t, Tensor), "mul requires a tensor or scalar"
 
-    # NOTE: consider whether operands should be validated
-    def add(self, t):
-        assert isinstance(t, Tensor)
+        def grad_fn():
+            a, b = (self, t) if self.data.ndim >= t.data.ndim else (t, self)
 
-        def add_grad():
-            self.grad = np.ones_like(self.data) * out.grad
-            t.grad = np.ones_like(t.data) * out.grad
+            a._grad += b.data * out.grad
+            b._grad += (a.data * out.grad).sum(
+                axis=tuple(i for i in range(a.data.ndim - b.data.ndim))
+            )
 
-            # TODO: not sure this is correct. I mean abstraction wise
-            if self.data.shape != t.data.shape:
-                if self.data.ndim > t.data.ndim:
-                    t.grad = t.grad.sum(axis=0)
-                else:
-                    self.grad = self.grad.sum(axis=0)
-
-        out = Tensor(self.data + t.data, grad_fn=add_grad, src=[self, t])
+        out = Tensor(np.multiply(self.data, t.data), grad_fn=grad_fn, src=[self, t])
 
         return out
 
-    def relu(self):
-        def relu_grad():
-            self.grad = (self.data > 0) * out.grad
+    # TODO: support higher-order tensors in `.dot` method
+    # TODO: implement a single `.dot` method that supports all cases allowed by numpy, and remove `.mv` and `.mm`
 
-        out = Tensor(np.maximum(self.data, 0), grad_fn=relu_grad, src=[self])
+    def dot(self, t):
+        assert isinstance(t, Tensor), "dot requires a tensor"
+        assert self.data.ndim == 1 and t.data.ndim == 1, "dot requires 1D tensors"
+
+        def grad_fn():
+            self._grad += t.data * out.grad
+            t._grad += self.data * out.grad
+
+        out = Tensor(np.dot(self.data, t.data), grad_fn=grad_fn, src=[self, t])
 
         return out
 
-    def sigmoid(self):
-        def sigmoid_grad():
-            self.grad = out.grad * out.data * (1 - out.data)
+    def mv(self, t):
+        assert isinstance(t, Tensor), "mv requires a tensor"
+        assert (self.data.ndim == 1 and t.data.ndim == 2) or (
+            self.data.ndim == 2 and t.data.ndim == 1
+        ), "mv requires 1D and 2D tensors"
 
-        out = Tensor(1 / (1 + np.exp(-self.data)), grad_fn=sigmoid_grad, src=[self])
+        def grad_fn():
+            mat, vec = (self, t) if self.data.ndim == 2 else (t, self)
+
+            # TODO: figure out a more straightforward way to find the gradients
+            if self is mat:
+                mat._grad += vec.data * out.grad.reshape(-1, 1)
+            else:
+                mat._grad += vec.data.reshape(-1, 1) * out.grad
+            vec._grad += np.dot(out.grad, mat.data if mat is self else mat.data.T)
+
+        out = Tensor(np.dot(self.data, t.data), grad_fn=grad_fn, src=[self, t])
 
         return out
 
-    # NOTE: consider whether operands should be validated
-    def mean_squared_error(self, y):
-        assert isinstance(y, Tensor)
+    def mm(self, t):
+        assert isinstance(t, Tensor), "mm requires a tensor"
+        assert self.data.ndim == 2 and t.data.ndim == 2, "mm requires 2D tensors"
 
-        def mse_grad():
-            self.grad = (self.data - y.data) * out.grad
-            y.grad = (y.data - self.data) * out.grad
+        def grad_fn():
+            self._grad += np.matmul(out.grad, t.data.T)
+            t._grad += np.matmul(self.data.T, out.grad)
 
-        out = Tensor(
-            (np.square(self.data - y.data) / 2).sum(axis=1).mean(keepdims=True),
-            grad_fn=mse_grad,
-            src=[self, y],
-        )
+        out = Tensor(np.matmul(self.data, t.data), grad_fn=grad_fn, src=[self, t])
+
+        return out
+
+    def sum(self):
+        def grad_fn():
+            self._grad += np.ones_like(self._data) * out._grad
+
+        out = Tensor(np.sum(self._data), grad_fn=grad_fn, src=[self])
 
         return out
 
     def backward(self):
-        if self._grad_fn is None:
-            return
+        assert self.data.shape == (), "backward only supported for scalar outputs"
 
-        if self.grad is None:
-            self.grad = 1
+        def backward(t):
+            if t._grad_fn is None:
+                return
 
-        self._grad_fn()
+            t._grad_fn()
 
-        if self._src is not None:
-            for t in self._src:
-                t.backward()
+            if t._src is not None:
+                for t in t._src:
+                    backward(t)
+
+        self._grad = 1
+
+        backward(self)
